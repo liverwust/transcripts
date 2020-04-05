c85a11422066d0ad254c3175c4e90bfbf16eaa1b
Louis Wust <louiswust@fastmail.fm>
2020-04-05 15:11:01 -0400

    Implement test from previous commit

--- adjust_mtimes.sh
+++ adjust_mtimes.sh
@@ -12,10 +12,12 @@ output_directory=${ADJUST_MTIME_BATS_OUTPUTDIR:-/tmp/macro-output}
 ogginfo=${ADJUST_MTIME_BATS_OGGINFO:-ogginfo}
 
 input_listing=`mktemp`
+input_listing_without_extensions=`mktemp`
 output_listing=`mktemp`
 
 # Input is assumed to be sorted alphabetically
 ls -1 "$input_directory" > "$input_listing"
+sed -r 's/\.[a-zA-Z0-9]+$//' < "$input_listing" > "$input_listing_without_extensions"
 # Output sorting is based on oldest (1st processed) to newest (last)
 ls -1 -tr "$output_directory" | tail -n `cat "$input_listing" | wc -l` > "$output_listing"
 
@@ -23,6 +25,11 @@ ls -1 -tr "$output_directory" | tail -n `cat "$input_listing" | wc -l` > "$outpu
 if head -n1 "$input_listing" | grep -q -v CarNoise
 then
 	echo "CarNoise should have been the first file processed" >/dev/stderr
+	exit 1
+fi
+if [ `sort -u "$input_listing_without_extensions" | wc -l` -lt `cat "$input_listing" | wc -l` ]
+then
+	echo "One or more files have duplicate filenames, excluding the extension" >/dev/stderr
 	exit 1
 fi
 if $ogginfo `head -n1 "$output_listing"` | grep -qE 'data length: +[0-9]{5,}'
@@ -47,16 +54,17 @@ fi
 # Everything looks good, so go ahead and transfer file attributes
 seq 1 `cat "$input_listing" | wc -l` | while read i; do
 	input_filename=`head -n$i "$input_listing" | tail -n1`
+	input_filename_wo_ext=`head -n$i "$input_listing_without_extensions" | tail -n1`
 	output_filename=`head -n$i "$output_listing" | tail -n1`
 	if [ $i -eq 1 ]; then
 		# Remove the otherwise useless CarNoise file from output
 		rm "$output_directory/$output_filename"
 	else
-		input_filename_wo_ext=`echo "$input_filename" | sed -r "s/\\.[a-zA-Z0-9]+\$//"`
 		touch -r "$input_directory/$input_filename" "$output_directory/$output_filename"
 		mv "$output_directory/$output_filename" "$output_directory/${input_filename_wo_ext}_processed.ogg"
 	fi
 done
 
 rm "$input_listing"
+rm "$input_listing_without_extensions"
 rm "$output_listing"
